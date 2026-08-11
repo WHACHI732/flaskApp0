1bd1306924ad7c44bc7e95c49f4e74ddf26e4d39
WHACHI732 <netbum21@gmail.com>
2026-08-11 22:52:35 +0700

    feat(api): serve phonebook data as JSON

--- app/views.py
+++ app/views.py
@@ -1,5 +1,20 @@
 from app import app
+from flask import jsonify
 
 @app.route('/')
 def home():
-    return "Hello world!"
+    return "WHACHI says Hello world!"
+
+@app.route('/phonebook')
+def index():
+    return app.send_static_file('phonebook.html')
+
+@app.route('/api/data')
+def data():
+
+    d = {
+        "Alice": "(708) 727_2377",
+        "Whachi": "0610527443"
+    }
+
+    return jsonify(d)
